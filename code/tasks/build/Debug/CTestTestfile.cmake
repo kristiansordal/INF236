549661b785e46e74af2b3aeada3fab562f038cd4
Kristian Sørdal <krisor99@gmail.com>
2024-01-18 13:19:49 +0100

    init

--- code/tasks/build/Debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kristiansordal/dev/uib/spring24/inf236/code/tasks
-# Build directory: /Users/kristiansordal/dev/uib/spring24/inf236/code/tasks/build/Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[week1]=] "/Users/kristiansordal/dev/uib/spring24/inf236/code/tasks/build/Debug/tests")
-set_tests_properties([=[week1]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/kristiansordal/dev/uib/spring24/inf236/code/tasks/CMakeLists.txt;52;add_test;/Users/kristiansordal/dev/uib/spring24/inf236/code/tasks/CMakeLists.txt;0;")
-subdirs("_deps/unity-build")
--- code/tasks/build/Debug/_deps/unity-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kristiansordal/dev/uib/spring24/inf236/code/tasks/build/Debug/_deps/unity-src
-# Build directory: /Users/kristiansordal/dev/uib/spring24/inf236/code/tasks/build/Debug/_deps/unity-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
