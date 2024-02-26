e8d96d9649eb8424b0af4ec3f6192613e0f05028
Kristian Sørdal <krisor99@gmail.com>
2024-02-26 11:58:31 +0100

    basically im stuff

--- code/assignments/radix/src/main.c
+++ code/assignments/radix/src/main.c
@@ -5,29 +5,44 @@
 #include "radix_seq.h"
 #endif
 #include <stdio.h>
+
+void input(int *size, int *bits) {
+    printf("Enter the size of the array: ");
+    scanf("%d", size);
+    printf("Enter the key size: ");
+    scanf("%d", bits);
+}
+
 int main() {
     int size, bits;
-    printf("Enter the size of the array: ");
-    scanf("%d", &size);
-    printf("Enter the key size: ");
-    scanf("%d", &bits);
-    printf("Elements: %d, key size: %d\n", size, bits);
+    double par_time, seq_time;
 
-#if defined(SEQ) || defined(COMPARE)
-    double seq_time = radix_sort_seq(size, bits);
+#ifdef SEQ
+    input(&size, &bits);
+    seq_time = radix_sort_seq(size, bits);
     printf("SEQUENTIAL time: %f\n", seq_time);
 #endif
-#if defined(PAR) || defined(COMPARE)
-    double par_time = radix_sort_par(size, bits);
+#ifdef PAR
+    input(&size, &bits);
+    par_time = radix_sort_par(size, bits);
     printf("PARALLEL time: %f\n", par_time);
 #endif
 
 #ifdef COMPARE
-    double speedup_ratio = seq_time / par_time;
-    double speedup_percentage = (speedup_ratio - 1.0) * 100.0;
+    int keys[5] = {1, 2, 4, 8, 16};
+    int elems[10] = {1e5, 1e6, 1e7, 1e8};
 
-    printf("Speedup ratio: %.3f\n", speedup_ratio);
-    printf("Speedup prcnt: %.3f%%\n", speedup_percentage);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("===================================\n");
+            printf("Key size: %d, Array size: %d\n", keys[i], elems[j]);
+            seq_time = radix_sort_seq(elems[j], keys[i]);
+            par_time = radix_sort_par(elems[j], keys[i]);
+            double speedup_ratio = seq_time / par_time;
+            double speedup_percentage = (speedup_ratio - 1.0) * 100.0;
+            printf("Speedup prcnt: %.3f%%\n", speedup_percentage);
+        }
+    }
 #endif
 
     return 0;
--- code/assignments/radix/src/radix_par.c
+++ code/assignments/radix/src/radix_par.c
@@ -1,32 +1,28 @@
 #include "radix_par.h"
-#include "mt19937-64.h"
 #include "util.h"
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void parallel_inclusive_scan(int *bucket_size, int *sums, const int NUM_BUCKETS, int p) {
+void parallel_inclusive_scan(int *bs, int *sums, const int buckets, int p) {
 
     // FALLBACK: If number of buckets is smaller than amount of threads, just use normal PFS
-    if (p > NUM_BUCKETS) {
+    if (p > buckets) {
         printf("Fallback on normal pfs\n");
-        inclusive_scan(bucket_size, NUM_BUCKETS);
+        inclusive_scan(bs, buckets);
         return;
     }
 
-    // ull *sums = malloc(p * sizeof(ull));
-
 #pragma omp parallel
     {
-
         int thread = omp_get_thread_num();
-        int start = (thread * NUM_BUCKETS) / p;
-        int end = thread == p - 1 ? NUM_BUCKETS : ((thread + 1) * NUM_BUCKETS) / p;
+        int start = (thread * buckets) / p;
+        int end = thread == p - 1 ? buckets : ((thread + 1) * buckets) / p;
 
-        for (int i = start + 1; i < end; i++) {
-            bucket_size[i] += bucket_size[i - 1];
-        }
-        sums[thread] = bucket_size[end - 1];
+        for (int i = start + 1; i < end; i++)
+            bs[i] += bs[i - 1];
+
+        sums[thread] = bs[end - 1];
     }
 
     for (int i = 1; i < p; i++) {
@@ -36,19 +32,18 @@ void parallel_inclusive_scan(int *bucket_size, int *sums, const int NUM_BUCKETS,
 #pragma omp parallel
     {
         int thread = omp_get_thread_num();
-        int start = (thread * NUM_BUCKETS) / p;
-        int end = thread == p - 1 ? NUM_BUCKETS : ((thread + 1) * NUM_BUCKETS) / p;
+        int start = (thread * buckets) / p;
+        int end = thread == p - 1 ? buckets : ((thread + 1) * buckets) / p;
         int addend = thread > 0 ? sums[thread - 1] : 0;
         for (int i = start; i < end; i++) {
             if (thread > 0)
-                bucket_size[i] += addend;
-        }
-    }
-}
+                bs[i] += addend;
 
-void inclusive_scan(int *bucket_size, const int NUM_BUCKETS) {
-    for (int i = 1; i < NUM_BUCKETS; i++) {
-        bucket_size[i] += bucket_size[i - 1];
+#pragma omp single
+            { printf("%d ", bs[i]); }
+        }
+#pragma omp single
+        { printf("\n"); }
     }
 }
 
@@ -56,8 +51,8 @@ void inclusive_scan(int *bucket_size, const int NUM_BUCKETS) {
 double radix_sort_par(int n, int b) {
     ull *a = (ull *)malloc(n * sizeof(ull));
     ull *tmp = (ull *)malloc(n * sizeof(ull));
-    double pfs_time = 0, tmp_time = 0, bucket_time = 0;
-    double t1, t2, t3;
+    const int buckets = 1 << b;
+    int bs[buckets];
     int p;
 
 #pragma omp parallel
@@ -66,62 +61,38 @@ double radix_sort_par(int n, int b) {
         { p = omp_get_num_threads(); }
     }
 
-    int *sums = malloc(p * sizeof(int));
+    // int lbs[p][buckets];
 
     // Generate random 64 bit integers
-    for (int i = 0; i < n; i++) {
-        a[i] = genrand64_int64();
-    }
+    init_rand_par(a, n);
 
     const double start = omp_get_wtime();
-    const int NUM_BUCKETS = 1 << b;
-    int bucket_size[NUM_BUCKETS];
 
-    // For each bit in the number, LSD order
     for (int shift = 0; shift < BITS; shift += b) {
-
-        t3 = omp_get_wtime();
-#pragma omp parallel for
-        for (int i = 0; i < NUM_BUCKETS; i++) {
-            bucket_size[i] = 0;
-        }
+        int_init_par(bs, buckets);
 
 #pragma omp parallel
         {
-            int local_bucket_size[NUM_BUCKETS];
-            for (int i = 0; i < NUM_BUCKETS; i++) {
-                local_bucket_size[i] = 0;
-            }
+            int lb[buckets];
+            int_init_par(lb, buckets);
 
-#pragma omp for nowait
-            for (int i = 0; i < n; i++) {
-                int bucket = (a[i] >> shift) & (NUM_BUCKETS - 1);
-                local_bucket_size[bucket]++;
-            }
+#pragma omp for
+            for (int i = 0; i < n; i++)
+                lb[(a[i] >> shift) & (buckets - 1)]++;
 
 #pragma omp critical
             {
-                for (int i = 0; i < NUM_BUCKETS; i++) {
-                    bucket_size[i] += local_bucket_size[i];
-                }
+                for (int i = 0; i < buckets; i++)
+                    bs[i] += lb[i];
             }
         }
-        t3 = omp_get_wtime() - t3;
-        bucket_time += t3;
 
-        // Prefix sum to compute the start of each bucket
-        t1 = omp_get_wtime();
-        parallel_inclusive_scan(bucket_size, sums, NUM_BUCKETS, p); // somehow slower than sequential pfs?
-        t1 = omp_get_wtime() - t1;
-        pfs_time += t1;
+        inclusive_scan(bs, buckets);
 
-        t2 = omp_get_wtime();
+        // Place the bit sorted elements back into the array
         for (int i = n - 1; i >= 0; i--) {
-            int bucket = (a[i] >> shift) & (NUM_BUCKETS - 1);
-            tmp[--bucket_size[bucket]] = a[i];
+            tmp[--bs[(a[i] >> shift) & (buckets - 1)]] = a[i];
         }
-        t2 = omp_get_wtime() - t2;
-        tmp_time += t2;
 
         ull *swap = a;
         a = tmp;
@@ -136,14 +107,12 @@ double radix_sort_par(int n, int b) {
         }
     }
 
-    printf("PFS TIME: %f\n", pfs_time);
-    printf("TMP TIME: %f\n", tmp_time);
-    printf("BUCKET TIME: %f\n", bucket_time);
-    int ans = is_sorted(a, n);
-    if (ans == 1)
+#ifdef PAR
+    if (is_sorted(a, n) == 1)
         printf("PARALLEL: Success!\n");
     else
         printf("PARALLEL: Failed!\n");
+#endif
 
     free(a);
     return end - start;
