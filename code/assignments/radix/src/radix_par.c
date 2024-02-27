8de85f0e60f520319e535faf7cca779cb179ef42
Kristian Sørdal <krisor99@gmail.com>
2024-02-27 11:54:10 +0100

    ok might be hella fast

--- code/assignments/radix/src/radix_par.c
+++ code/assignments/radix/src/radix_par.c
@@ -4,55 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void parallel_inclusive_scan(int *bs, int *sums, const int buckets, int p) {
-
-    // FALLBACK: If number of buckets is smaller than amount of threads, just use normal PFS
-    if (p > buckets) {
-        printf("Fallback on normal pfs\n");
-        inclusive_scan(bs, buckets);
-        return;
-    }
-
-#pragma omp parallel
-    {
-        int thread = omp_get_thread_num();
-        int start = (thread * buckets) / p;
-        int end = thread == p - 1 ? buckets : ((thread + 1) * buckets) / p;
-
-        for (int i = start + 1; i < end; i++)
-            bs[i] += bs[i - 1];
-
-        sums[thread] = bs[end - 1];
-    }
-
-    for (int i = 1; i < p; i++) {
-        sums[i] += sums[i - 1];
-    }
-
-#pragma omp parallel
-    {
-        int thread = omp_get_thread_num();
-        int start = (thread * buckets) / p;
-        int end = thread == p - 1 ? buckets : ((thread + 1) * buckets) / p;
-        int addend = thread > 0 ? sums[thread - 1] : 0;
-        for (int i = start; i < end; i++) {
-            if (thread > 0)
-                bs[i] += addend;
-
-#pragma omp single
-            { printf("%d ", bs[i]); }
-        }
-#pragma omp single
-        { printf("\n"); }
-    }
-}
-
 // Parallel radix sort
 double radix_sort_par(int n, int b) {
     ull *a = (ull *)malloc(n * sizeof(ull));
-    ull *tmp = (ull *)malloc(n * sizeof(ull));
+    ull *permuted = (ull *)malloc(n * sizeof(ull));
     const int buckets = 1 << b;
-    int bs[buckets];
     int p;
 
 #pragma omp parallel
@@ -61,42 +17,59 @@ double radix_sort_par(int n, int b) {
         { p = omp_get_num_threads(); }
     }
 
-    // int lbs[p][buckets];
+    int histogram[p][buckets]; // Sub-bucket-size table
+    int bs[buckets];           // Bucket size table
+    int pfs[buckets];          // Prefix sum table
+    ull out_buf[p][buckets];
 
     // Generate random 64 bit integers
     init_rand_par(a, n);
-
     const double start = omp_get_wtime();
 
     for (int shift = 0; shift < BITS; shift += b) {
-        int_init_par(bs, buckets);
+#pragma omp parallel
+        {
+            const int tid = omp_get_thread_num();
+            int_init_par(histogram[tid], buckets);
+            int_init_par(bs, buckets);
+            int_init_par(pfs, buckets);
+
+#pragma omp for nowait
+            for (int i = 0; i < n; i++)
+                histogram[tid][(a[i] >> shift) & (buckets - 1)]++;
+        }
+
+        int s = 0;
+        for (int i = 0; i < buckets; i++) {
+            for (int j = 0; j < p; j++) {
+                const int t = s + histogram[j][i];
+                histogram[j][i] = s;
+                s = t;
+            }
+            bs[i] = s;
+        }
 
 #pragma omp parallel
         {
-            int lb[buckets];
-            int_init_par(lb, buckets);
+            const int tid = omp_get_thread_num();
+            int *histo_tid = histogram[tid];
 
 #pragma omp for
-            for (int i = 0; i < n; i++)
-                lb[(a[i] >> shift) & (buckets - 1)]++;
+            for (int i = 0; i < n; i++) {
+                ull val = a[i];                         // get value
+                int t = (val >> shift) & (buckets - 1); // get bucket
+                // ++histo_tid[t];                         // index in permuted
+                permuted[histo_tid[t]++] = val;
 
-#pragma omp critical
-            {
-                for (int i = 0; i < buckets; i++)
-                    bs[i] += lb[i];
+                // when buffer is full
+                // loop through histo tid
+                // dist[p++] = my_buf[t][j]
             }
         }
 
-        inclusive_scan(bs, buckets);
-
-        // Place the bit sorted elements back into the array
-        for (int i = n - 1; i >= 0; i--) {
-            tmp[--bs[(a[i] >> shift) & (buckets - 1)]] = a[i];
-        }
-
         ull *swap = a;
-        a = tmp;
-        tmp = swap;
+        a = permuted;
+        permuted = swap;
     }
 
     const double end = omp_get_wtime();
@@ -107,13 +80,16 @@ double radix_sort_par(int n, int b) {
         }
     }
 
-#ifdef PAR
     if (is_sorted(a, n) == 1)
         printf("PARALLEL: Success!\n");
     else
         printf("PARALLEL: Failed!\n");
-#endif
 
     free(a);
     return end - start;
 }
+
+// for (int i = n - 1; i >= 0; i--) {
+//     int bucket = (a[i] >> shift) & (buckets - 1);
+//     permuted[--bs[bucket]] = a[i];
+// }
--- code/assignments/radix/src/radix_par.h
+++ code/assignments/radix/src/radix_par.h
@@ -1,4 +1,2 @@
 #pragma once
 double radix_sort_par(int n, int b);
-void parallel_inclusive_scan(int *bs, int *sums, const int buckets, int p);
-void inclusive_scan(int *bs, const int buckets);
