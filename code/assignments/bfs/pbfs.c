af76304724feb9bd02bc0fd8a28f1284330307bf
Kristian Sørdal <krisor99@gmail.com>
2024-03-17 15:51:14 +0100

    asdf

--- code/assignments/bfs/pbfs.c
+++ code/assignments/bfs/pbfs.c
@@ -20,41 +20,56 @@
 // Note that the vertices are numbered from 1 to n (inclusive). Thus there is
 // no vertex 0.
 
+#include <stdlib.h>
 void pbfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
-    int i, j;         // Loop indices
-    int v, w;         // Pointers to vertices
-    int num_r, num_w; // Number of vertices in S and T, respectively
-    int *temp;        // Temporary pointer
+    int i, j;
+    int v, w;
+    int num_r, num_w;
+    int *temp;
+    int *T_local = malloc(n * sizeof(int));
+    int local_w;
 
-    for (i = 1; i <= n; i++) { // Set that every node is unvisited
-        p[i] = -1;             // Using -1 to mark that a vertex is unvisited
+    for (i = 1; i <= n; i++) {
+        p[i] = -1;
         dist[i] = -1;
     }
 
-    p[1] = 1;    // Set the parent of starting vertex to itself
-    dist[1] = 0; // Set the distance from the starting vertex to itself
-    S[0] = 1;    // Add the starting vertex to S
+    p[1] = 1;
+    dist[1] = 0;
+    S[0] = 1;
 
-    num_r = 1; // Number of vertices in S
-    num_w = 0; // Number of vertices in T
+    num_r = 1;
+    num_w = 0;
 
     while (num_r != 0) {
+        local_w = 0;
         for (i = 0; i < num_r; i++) {
             v = S[i];
 #pragma omp for
-            for (j = ver[v]; j < ver[v + 1]; j++) { // Go through the neighbors of v
-                w = edges[j];                       // Get next neighbor w of v
-                if (p[w] == -1) {                   // Check if w is undiscovered
-                    p[w] = v;                       // Set v as the parent of w
-                    dist[w] = dist[v] + 1;          // Set distance of w
-                    T[num_w++] = w;                 // Add w to T and increase number of vertices discovered
+            for (j = ver[v]; j < ver[v + 1]; j++) {
+                w = edges[j];
+                if (p[w] == -1) {
+                    p[w] = v;
+                    dist[w] = dist[v] + 1;
+                    T_local[local_w++] = w;
                 }
-            }     // End loop over neighbors of v
-        }         // End loop of vertices in S
-        temp = S; // Swap S and T
-        S = T;
-        T = temp;
-        num_r = num_w; // Set number of elements in S
-        num_w = 0;     // Set T as empty
-    }                  //  End loop over entire graph
+            }
+
+#pragma omp critical
+            {
+                for (i = 0; i < local_w; i++)
+                    T[num_w++] = T_local[i];
+            }
+        }
+
+#pragma omp master
+        {
+            temp = S;
+            S = T;
+            T = temp;
+
+            num_r = num_w;
+            num_w = 0;
+        }
+    }
 }
