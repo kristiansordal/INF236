a370390113d41f85a5ec5999b060fc82d9448533
Kristian Sørdal <krisor99@gmail.com>
2024-03-18 12:18:01 +0100

    adsf

--- code/assignments/bfs/pbfs.c
+++ code/assignments/bfs/pbfs.c
@@ -50,16 +50,14 @@ void pbfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
 #pragma omp for
         for (int i = 0; i < layer_size; i++) {
             int v = S[i];
-
             for (int j = ver[v]; j < ver[v + 1]; j++) {
                 int u = edges[j];
-                printf("u: %d\n", u);
 
                 if (p[u] == -1) {
                     p[u] = v;
                     dist[u] = dist[v] + 1;
                     discovered[tid][local_u++] = u;
-                    printf("discovered[%d][%d]: %d\n", tid, local_u - 1, discovered[tid][local_u - 1]);
+                    printf("discovered %d\n", u);
                 }
             }
         }
