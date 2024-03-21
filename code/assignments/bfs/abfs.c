f442271a7fd8c9c38ce0e02df682680a4de3a6a0
Kristian Sørdal <krisor99@gmail.com>
2024-03-21 09:05:54 +0100

    asdf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -101,8 +101,7 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
     {
         T[1] = sequential_k_steps(n, ver, edges, p, dist, S, T, seq_limit, &start_idx);
         T[0] = start_idx; // need to store this in a shared variable
-        printf("Start: %d and %d\n", start_idx, T[0]);
-        printf("Layer size: %d\n", T[1]);
+        printf("To distribute: %d -> %d\n", T[0], T[1]);
     }
 #pragma omp barrier
 
