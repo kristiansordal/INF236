31a5a7f37dc35eab97d60b20f6bd87dcd1e08986
Kristian Sørdal <krisor99@gmail.com>
2024-03-21 15:05:54 +0100

    asdf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -143,7 +143,7 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
             int chunk = layer_size / threads;
             int start = chunk * tid;
             int end = tid == threads - 1 ? layer_size : chunk * (tid + 1);
-            printf("Tid %d: %d -> %d\n", tid, start, end);
+            // printf("Tid %d: %d -> %d\n", tid, start, end);
             local_layer_size = end - start;
             for (int i = start; i < end; i++)
                 local_S[i - start] = S[i];
