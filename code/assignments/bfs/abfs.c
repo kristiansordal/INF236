e8b27340fe1bc9ab40d845454ad333f70286b115
Kristian Sørdal <krisor99@gmail.com>
2024-03-22 15:11:08 +0100

    asdf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -64,7 +64,7 @@ int sequential_steps(int n, int *ver, int *edges, int *p, int *dist, int *S, int
         T = temp;
         layer_size = num_discovered;
         flips++;
-        printf("layer: %d", flips);
+        printf("layer: %d\n", flips);
 
         num_discovered = 0;
     }
