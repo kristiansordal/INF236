72d904168f99812911c6ea600f29c7f513ce4e52
Kristian Sørdal <krisor99@gmail.com>
2024-03-22 15:18:01 +0100

    adsf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -142,11 +142,11 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
             int new_dist = dist[v] + 1;
             for (int j = ver[v]; j < ver[v + 1]; j++) {
                 int u = edges[j];
-                // if (tid == 2 && depth == 0) {
-                //     printf("discovered: %d, dist: %d, correct parent: %d, current parent: %d\n", u, new_dist, v,
-                //     p[u]);
-                // }
-                if (p[u] == -1) {
+                if (p[u] == -1 || dist[u] > new_dist) {
+                    if (tid == 2 && depth == 0) {
+                        printf("discovered: %d, dist: %d, correct parent: %d, current parent: %d\n", u, new_dist, v,
+                               p[u]);
+                    }
                     p[u] = v;
                     dist[u] = new_dist;
                     discovered[num_discovered++] = u;
