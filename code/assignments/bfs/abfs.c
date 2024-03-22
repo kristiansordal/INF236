37202089c964b8e0574869b1678248d588f6098c
Kristian Sørdal <krisor99@gmail.com>
2024-03-22 14:48:18 +0100

    asdf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -103,8 +103,10 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
     { T[0] = sequential_steps(n, ver, edges, p, dist, S, T); }
 #pragma omp barrier
 
-    for (int i = 0; i < 100; i++) {
-        printf("p[%d]: %d, dist[%d]: %d\n", i, p[i], i, dist[i]);
+#pragma omp single
+    {
+        for (int i = 0; i < 50; i++)
+            printf("p[%d]: %d, dist[%d]: %d\n", i, p[i], i, dist[i]);
     }
     // populate local_S
     int chunk = T[0] / threads;
