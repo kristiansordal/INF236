5ceaa8c0a8a32f768ed28e15b77733b57aadb766
Kristian Sørdal <krisor99@gmail.com>
2024-03-22 10:39:44 +0100

    adsf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -123,12 +123,11 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
 #pragma omp critical
                     {
                         if (p[u] == -1) {
-
                             p[u] = v;
                             dist[u] = dist[v] + 1;
-                            discovered[num_discovered++] = u;
                         }
                     }
+                    discovered[num_discovered++] = u;
                 }
             }
         }
