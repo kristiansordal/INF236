b2538120b2ee17f368f6d7c007b6052fb8bd8c35
Kristian Sørdal <krisor99@gmail.com>
2024-03-22 10:38:35 +0100

    adf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -53,14 +53,9 @@ int sequential_steps(int n, int *ver, int *edges, int *p, int *dist, int *S, int
             for (int j = ver[v]; j < ver[v + 1]; j++) {
                 int u = edges[j];
                 if (p[u] == -1) {
-#pragma omp critical
-                    {
-                        if (p[u] == -1) {
-                            p[u] = v;
-                            dist[u] = dist[v] + 1;
-                            T[num_discovered++] = u;
-                        }
-                    }
+                    p[u] = v;
+                    dist[u] = dist[v] + 1;
+                    T[num_discovered++] = u;
                 }
             }
         }
@@ -123,10 +118,17 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
             int v = local_S[i];
             for (int j = ver[v]; j < ver[v + 1]; j++) {
                 int u = edges[j];
+
                 if (p[u] == -1) {
-                    p[u] = v;
-                    dist[u] = depth + 1;
-                    discovered[num_discovered++] = u;
+#pragma omp critical
+                    {
+                        if (p[u] == -1) {
+
+                            p[u] = v;
+                            dist[u] = depth + 1;
+                            discovered[num_discovered++] = u;
+                        }
+                    }
                 }
             }
         }
