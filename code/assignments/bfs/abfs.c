4de81962640690966bded31a2664c4b56f2b4901
Kristian Sørdal <krisor99@gmail.com>
2024-03-23 15:24:28 +0100

    asdf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -76,7 +76,6 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
     while (l_tot != 0) {
         for (int i = 0; i < k; i++) {
 #pragma omp barrier
-            printf("Thread %d, layer %d\n", tid, l);
             for (int j = 0; j < l; j++) {
                 u = queue[j];
                 for (int w = ver[u]; w < ver[u + 1]; w++) {
