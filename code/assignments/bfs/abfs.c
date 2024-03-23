98b8450373b9f162bd5c809114b2c006e30cf68f
Kristian Sørdal <krisor99@gmail.com>
2024-03-23 14:22:13 +0100

    asdf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -104,7 +104,9 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
             l_tot += T[i];
         }
 #pragma omp barrier
-        memcpy(S + offset, queue, l * sizeof(int));
+        if (d > 0) {
+            memcpy(S + offset, queue, l * sizeof(int));
+        }
 #pragma omp barrier
 
         l = 0;
