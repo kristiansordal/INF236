468bed2c0893a44affd0b5e7552a948bdc044b0f
Kristian Sørdal <krisor99@gmail.com>
2024-03-23 13:08:33 +0100

    asdf

--- code/assignments/bfs/abfs.c
+++ code/assignments/bfs/abfs.c
@@ -75,17 +75,16 @@ void abfs(int n, int *ver, int *edges, int *p, int *dist, int *S, int *T) {
 
     // Perform some rounds of sequential BFS
 #pragma omp master
-    {
-        T[0] = sequential_steps(n, ver, edges, p, dist, S, T);
-        for (int i = 0; i < T[0]; i++) {
-            printf("Distance to root: %d\n", dist[S[i]]);
-        }
-    }
+    { T[0] = sequential_steps(n, ver, edges, p, dist, S, T); }
 #pragma omp barrier
 
 #pragma omp for
     for (int i = 0; i < T[0]; i++)
         queue[l++] = S[i];
+
+    for (int i = 0; i < T[0]; i++) {
+        printf("Distance to root: %d\n", dist[S[i]]);
+    }
 
     for (int i = 0; i < l; i++)
         printf("Initial: %d\n", queue[i]);
