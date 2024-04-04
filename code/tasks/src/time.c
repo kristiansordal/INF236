c64ec9c1a3c6c661042c6cb5fcfbb8539e11135d
Kristian Sørdal <krisor99@gmail.com>
2024-04-04 11:36:18 +0200

    asdf

--- code/tasks/src/time.c
+++ code/tasks/src/time.c
@@ -13,23 +13,26 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    int send_size;
-    scanf("%d", &send_size);
+    int send_size = 0;
+    if (rank == 0) {
+        scanf("%d", &send_size);
+        MPI_Send(&send_size, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    } else
+        MPI_Recv(&send_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
     int *x = malloc(send_size * sizeof(int));
     memset(x, 0, send_size * sizeof(int));
 
-    for (int i = 0; i < send_size; i++) {
-        double time = 0;
-        if (rank == 0) {
-            time = MPI_Wtime();
-            MPI_Send(x, i, MPI_INT, 1, 0, MPI_COMM_WORLD);
-            time = MPI_Wtime() - time;
-            printf("Sending %d elements took %f seconds\n", i, time);
+    double time = 0;
+    if (rank == 0) {
+        time = MPI_Wtime();
+        MPI_Send(x, send_size, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        time = MPI_Wtime() - time;
+        printf("Sending %d elements took %f seconds\n", send_size, time);
 
-        } else {
-            sleep(1);
-            MPI_Recv(x, i, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+    } else {
+        sleep(1);
+        MPI_Recv(x, send_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 
     MPI_Finalize();
