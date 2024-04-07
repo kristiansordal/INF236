36902d47101fe684cc69458ead0ce052567478e2
Kristian Sørdal <krisor99@gmail.com>
2024-04-07 18:52:56 +0200

    adsf13;2u

--- code/tasks/src/band.c
+++ code/tasks/src/band.c
@@ -3,22 +3,22 @@
 
 #define size 1000000
 
+int main() {
+    int i, j;
+    double data[size];
+    clock_t start, end;
+    double sum;
 
-main() {
-  int i,j;
-  double data[size];
-  clock_t start,end;
-  double sum;
+    sum = 0;
 
-   sum = 0;
+    start = clock();
+    for (j = 0; j < 100; j++)
+        for (i = 0; i < size; i++)
+            sum = sum + data[i] * data[i + 1];
+    end = clock();
 
-   start = clock();
-   for(j=0;j<100;j++)
-     for(i=0;i<size;i++)
-       sum = sum + data[i]*data[i+1];
-   end = clock();
-
-   printf("Time taken %f \n",(end-start)/(float)CLOCKS_PER_SEC);
-   printf("The sum is %d \n",sum);
-   printf("Time per element %10.9f \n",(end-start)/(float)CLOCKS_PER_SEC/(100*size));
+    printf("Time taken %f \n", (end - start) / (float)CLOCKS_PER_SEC);
+    printf("The sum is %d \n", sum);
+    printf("Time per element %10.9f \n", (end - start) / (float)CLOCKS_PER_SEC / (100 * size));
+    return 0;
 }
