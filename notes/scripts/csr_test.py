a09cff37605af6d1532721145fafa87c57dea89f
Kristian Sørdal <krisor99@gmail.com>
2024-05-04 17:30:39 +0200

    asdf

--- notes/scripts/csr_test.py
+++ notes/scripts/csr_test.py
@@ -1,36 +1,52 @@
 ti_sm, ti_sq, ti_na, ti_sh = 0, 0, 0, 0
 tr_sm, tr_sq, tr_na, tr_sh = 0, 0, 0, 0
 gf_sm, gf_sq, gf_na, gf_sh = 0, 0, 0, 0
-with open("csrout.txt") as f:
+with open("shared_out.txt") as f:
+    for line in f:
+        words = line.split()
+
+        if words[0] == "ti_sh":
+            ti_sh = float(words[1])
+        elif words[0] == "tr_sh":
+            tr_sh = float(words[1])
+        elif words[0] == "gf_sh":
+            gf_sh = float(words[1])
+f.close()
+
+with open("seq_out.txt") as f:
     for line in f:
         words = line.split()
 
         if words[0] == "ti_sq":
             ti_sq = float(words[1])
-        elif words[0] == "ti_sm":
-            ti_sm = float(words[1])
-        elif words[0] == "ti_na":
-            ti_na = float(words[1])
-        elif words[0] == "ti_sh":
-            ti_sh = float(words[1])
-
         elif words[0] == "tr_sq":
             tr_sq = float(words[1])
-        elif words[0] == "tr_sm":
-            tr_sm = float(words[1])
-        elif words[0] == "tr_na":
-            tr_na = float(words[1])
-        elif words[0] == "tr_sh":
-            tr_sh = float(words[1])
-
         elif words[0] == "gf_sq":
             gf_sq = float(words[1])
+f.close()
+
+with open("smart_out.txt") as f:
+    for line in f:
+        words = line.split()
+
+        if words[0] == "ti_sm":
+            ti_sm = float(words[1])
+        elif words[0] == "tr_sm":
+            tr_sm = float(words[1])
         elif words[0] == "gf_sm":
             gf_sm = float(words[1])
+f.close()
+
+with open("naive_out.txt") as f:
+    for line in f:
+        words = line.split()
+
+        if words[0] == "ti_na":
+            ti_na = float(words[1])
+        elif words[0] == "tr_na":
+            tr_na = float(words[1])
         elif words[0] == "gf_na":
             gf_na = float(words[1])
-        elif words[0] == "gf_sh":
-            gf_sh = float(words[1])
 f.close()
 
 with open("shared_res.txt", "a") as f:
